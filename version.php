9e6a460b9e302c78bdad8c1bf3b9eb0a488459d7
Jetha Chan <jetha@moodle.com>
2015-07-18 00:28:10 +0900

    Update required Moodle version so this can install on 2.8.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2015070902;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2015050500;        // Requires this Moodle version.
+$plugin->requires  = 2014111007.02;        // Requires this Moodle version.
 $plugin->component = 'atto_ruby';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_RC;
-$plugin->release = 'v0.1';
+$plugin->release = 'v0.2';
