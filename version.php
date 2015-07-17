d63cc5709beec89d0ac1b5d74007fc4b8c02dcf3
Jetha Chan <jetha@moodle.com>
2015-07-18 00:23:50 +0900

    Tag version.php with release information.

--- version.php
+++ version.php
@@ -27,3 +27,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2015070902;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2015050500;        // Requires this Moodle version.
 $plugin->component = 'atto_ruby';  // Full name of the plugin (used for diagnostics).
+$plugin->maturity = MATURITY_RC;
+$plugin->release = 'v0.1';
